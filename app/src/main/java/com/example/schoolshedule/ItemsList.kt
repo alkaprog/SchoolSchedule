b0d2e88216d234bd8ce3ac2d000ba688461832b4
alkaprog <superpupergametsar@gmail.com>
2021-11-19 12:37:02 +0300

    Items can be deleted by user

--- app/src/main/java/com/example/schoolshedule/ItemsList.kt
+++ app/src/main/java/com/example/schoolshedule/ItemsList.kt
@@ -1,14 +1,8 @@
 package com.example.schoolshedule
 
-import android.content.Intent
-import android.content.SharedPreferences
-import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.*
-import javax.security.auth.Subject
-import kotlin.math.log
 
 class ItemsList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,18 +44,21 @@ class ItemsList : AppCompatActivity() {
             var listOfUserItems = pref.getString(subjectName, "defaultName")?.split(",")
             if (listOfUserItems != null) {
                 for (item in listOfUserItems) {
-                    val tv = TextView(this)
-                    tv.text = item
-                    tv.textSize = 18F
+                    if (item.isNotEmpty()) {
+                        val tv = TextView(this)
+                        tv.text = item
+                        tv.textSize = 18F
 
-                    val params = LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.WRAP_CONTENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT
-                    )
-                    params.setMargins(50, 10, 0, 10)
-                    tv.layoutParams = params
+                        val params = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT
+                        )
+                        params.setMargins(50, 10, 0, 10)
+                        tv.layoutParams = params
 
-                    lv.addView(tv)
+                        lv.addView(tv)
+                    }
+
                 }
             }
         }
@@ -72,18 +69,36 @@ class ItemsList : AppCompatActivity() {
         lv.addView(textField)
 
 
-        var btn = Button(this)
-        btn.text = "Добавить предмет"
-        btn.setOnClickListener {
-            if (textField.text.toString()!="") {
+        var addItemButton = Button(this)
+        addItemButton.text = "Добавить предмет"
+        addItemButton.setOnClickListener {
+            if (textField.text.toString() != "") {
                 if (subjectName != null) {
-                    addAnItem(subjectName,textField.text.toString())
+                    addAnItem(subjectName, textField.text.toString())
                 }
             }
         }
-        lv.addView(btn)
+        lv.addView(addItemButton)
 
+        var deleteItemButton = Button(this)
+        deleteItemButton.text = "Удалить предмет"
+        deleteItemButton.setOnClickListener {
+            if (textField.text.toString() != "") {
+                if (subjectName != null) {
+                    deleteAnItem(subjectName, textField.text.toString())
+                }
+            }
+        }
+        lv.addView(deleteItemButton)
 
+        var deleteAllItemsButton = Button(this)
+        deleteAllItemsButton.text = "Удалить все предметы добавленные пользователем"
+        deleteAllItemsButton.setOnClickListener {
+            if (subjectName != null) {
+                deleteAllUserItems(subjectName)
+            }
+        }
+        lv.addView(deleteAllItemsButton)
     }
 
     private fun addAnItem(subjectName: String, itemName: String) {
@@ -99,7 +114,7 @@ class ItemsList : AppCompatActivity() {
                         if (row.isNotEmpty()) {
                             row += ",$itemName"
                         } else {
-                            row += ",$itemName"
+                            row += "$itemName"
                         }
                         editor.putString(subjectName, row)
                         editor.commit()
@@ -111,6 +126,56 @@ class ItemsList : AppCompatActivity() {
             editor.putString(subjectName, itemName)
             editor.commit()
         }
+        recreate()
+    }
+
+    private fun deleteAnItem(subjectName: String, itemName: String) {
+        var pref = getSharedPreferences("UserData", MODE_PRIVATE)
+        var editor = pref.edit()
+
+        if (pref.contains(subjectName)) {
+            var listOfUserItems = pref.getString(subjectName, "defaultName")?.split(",")
+
+            var row: String = ""
+
+            if (listOfUserItems != null) {
+                if (listOfUserItems.contains(itemName)) {
+                    for (item in listOfUserItems) {
+                        if (item != itemName) {
+                            if (row.isNotEmpty()) {
+                                row += ",$item"
+                            } else {
+                                row += "$item"
+                            }
+                        }
+                        /*
+                    if (row.length > 1) {
+                        if (row[row.length - 1] == ',') {
+                            row = row.substring(0, row.length - 1)
+                        }
+                    }
+
+                    if (row.length >= 0) {
+                        if (row[0] == ',') {
+                            row = row.substring(1)
+                        }
+                    }
+*/
+                        editor.putString(subjectName, row)
+                        editor.commit()
+                    }
+                }
+
+            }
+            recreate()
+        }
+
+    }
+
+    private fun deleteAllUserItems(subjectName: String) {
+        var pref = getSharedPreferences("UserData", MODE_PRIVATE)
+        var editor = pref.edit()
+        editor.remove(subjectName).commit()
         recreate()
     }
 }
